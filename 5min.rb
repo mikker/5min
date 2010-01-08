18c32dfc75f49377412b8fcf9682fc3538acff9c
Mikkel Malmberg <mikkelm@gmail.com>
2010-01-08 12:13:21 +0100

    use new bundler

--- 5min.rb
+++ 5min.rb
@@ -1,4 +1,13 @@
+# This makes sure the bundled gems are in our $LOAD_PATH
+require File.expand_path(File.join(File.dirname(__FILE__), 'vendor', 'gems', 'environment'))
+# This actually requires the bundled gems
+Bundler.require_env
+
 require "sinatra"
+
+configure do
+  set :root, File.dirname(__FILE__)
+end
 
 get '/' do
   erb :index
