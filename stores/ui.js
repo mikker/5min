7ef4fc33703a784d543b9d8f6ee38718c6850601
Mikkel Malmberg <mikkel@brnbw.com>
2018-02-23 11:39:12 +0100

    Set custom durations

--- stores/ui.js
+++ stores/ui.js
@@ -1,9 +1,15 @@
 function ui(state, emitter) {
-  state.ui = state.ui || {
-    showSettings: false,
-    soundOn: true,
-    darkMode: false
-  };
+  state.ui = Object.assign(
+    // start with the defaults
+    {
+      showSettings: false,
+      soundOn: true,
+      darkMode: false,
+      custom: { m: '', s: '' }
+    },
+    // then add whatever was persisted
+    state.ui
+  )
 
   emitter.on("ui:set", change => {
     Object.assign(state.ui, change);
